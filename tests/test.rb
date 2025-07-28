41c9be3766580c04576722241855685aa312159d
George MacKerron <george@mackerron.co.uk>
2025-07-28 12:51:00 +0100

    Tweaks

--- tests/test.rb
+++ tests/test.rb
@@ -15,10 +15,10 @@ end
 
 def with_postgres(port = 54320)
   puts '>> Generating TLS cert ...'
-  `openssl req -new -newkey rsa:4096 -nodes -out /tmp/ca.csr -keyout /tmp/ca.key -subj "/OU=Unknown/O=Unknown/L=Unknown/ST=Unknown/C=GB"
+  `openssl req -quiet -new -newkey rsa:4096 -nodes -out /tmp/ca.csr -keyout /tmp/ca.key -subj "/OU=Unknown/O=Unknown/L=Unknown/ST=Unknown/C=GB"
   openssl x509 -trustout -signkey /tmp/ca.key -days 2 -req -in /tmp/ca.csr -out /tmp/ca.pem
   openssl genrsa -out /tmp/client.key 4096
-  openssl req -new -key /tmp/client.key -out /tmp/client.csr -sha256 -subj "/OU=Unknown/O=Unknown/L=Unknown/ST=Unknown/C=GB"
+  openssl req -quiet -new -key /tmp/client.key -out /tmp/client.csr -sha256 -subj "/OU=Unknown/O=Unknown/L=Unknown/ST=Unknown/C=GB"
   openssl x509 -req -days 365 -in /tmp/client.csr -CA /tmp/ca.pem -CAkey /tmp/ca.key -out /tmp/client.cer`
 
   puts '>> Starting Docker Postgres ...'
@@ -74,15 +74,13 @@ rescue => err
   result = err
 ensure
   is_err = result.kind_of?(Exception)
+  puts result.full_message if is_err
+
   failed = is_err || !result
-  if failed
-    $fails += 1
-  else
-    $passes += 1
-  end
+  $passes += 1 unless failed
+  $fails += 1 if failed
 
   puts "#{failed ? "\033[31mFAIL" : "\033[32mPASS"}\033[0m  \033[1m#{desc}\033[0m"
-  puts Exception if is_err
 end
 
 begin
@@ -97,10 +95,9 @@ begin
       result && pgpi_log.include?('client -> server: "X" = Terminate "\x00\x00\x00\x04" = 4 bytes')
     end
 
-
-
   end
 ensure
   Process.waitall
   puts "\033[1m#{$passes} passed, #{$fails} failed\033[0m"
+  exit($fails == 0)
 end
